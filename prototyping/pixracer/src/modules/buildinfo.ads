ceb1b50940d363e4a323a4e8176d007a0f29c5ec
mbeckersys <mbeckersys@users.noreply.github.com>
2016-07-31 21:13:05 +0200

    restored broken softlinks

--- prototyping/pixracer/src/modules/buildinfo.ads
+++ prototyping/pixracer/src/modules/buildinfo.ads
@@ -1,8 +1,1 @@
-package Buildinfo is
-   function Compilation_Date return String -- implementation-defined (GNAT)
-     with Import, Convention => Intrinsic;
-   function Compilation_Time return String -- implementation-defined (GNAT)
-     with Import, Convention => Intrinsic;
-
-   function Short_Datetime return String;
-end Buildinfo;
+../../../../software/modules/buildinfo.ads
