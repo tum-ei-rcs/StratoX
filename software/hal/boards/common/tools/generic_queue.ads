ba2d47cebc771a817d45aeae1f9386a8b240502b
mbeckersys <mbeckersys@users.noreply.github.com>
2016-08-12 19:57:35 +0200

    SPARK fixes in generic_queue, moved unused units to other folder

--- software/hal/boards/common/tools/generic_queue.ads
+++ software/hal/boards/common/tools/generic_queue.ads
@@ -2,6 +2,13 @@
 -- Author: Emanuel Regnath (emanuel.regnath@tum.de)
 -- Can be used as ring buffer or queue
 
+-- Note: Since this used OO features (class-wide types), the Liskov Subsitution Principle (LSP)
+-- applies. That is, all contracts on primitives of the derived types must be *weaker*
+-- than the contracts given here, so that the derived types can always safely be substituted
+-- with the base type.
+-- For that reason, all contracts given here must apply to the class-wide type (denoted by
+-- Pre'Class, Post'Class, etc.)
+-- To avoid this, remove "tagged".
 generic
    type Index_Type is mod <>;
    type Element_Type is private;
@@ -16,66 +23,78 @@ package Generic_Queue with SPARK_Mode is
 
    type Buffer_Tag is tagged private;
 
-      procedure clear( Self : in out Buffer_Tag ) with post => Self.Empty;
-      -- remove all elements
+   procedure clear( Self : in out Buffer_Tag )
+     with Post'Class => Self.Empty;
+   -- remove all elements
 
-      procedure fill( Self : in out Buffer_Tag );
+   procedure fill( Self : in out Buffer_Tag );
 
-      procedure push_back( Self : in out Buffer_Tag; element : Element_Type ) with post => not Self.Empty;
-      -- append new element at back
+   procedure push_back( Self : in out Buffer_Tag; element : Element_Type )
+     with Post => not Self.Empty;
+   -- append new element at back
 
-      procedure push_front( Self : in out Buffer_Tag; element : Element_Type ) with post => not Self.Empty;
-      -- prepend new element at front
+   procedure push_front( Self : in out Buffer_Tag; element : Element_Type )
+     with Post'Class => not Self.Empty;
+   -- prepend new element at front
 
-      procedure pop_front( Self : in out Buffer_Tag; element : out Element_Type ) with pre => not Self.Empty, post => not Self.Full;
-      -- read and remove element at front
+   procedure pop_front( Self : in out Buffer_Tag; element : out Element_Type )
+     with Pre'Class => not Self.Empty,
+          Post'Class => not Self.Full;
+   -- read and remove element at front
 
-      procedure pop_front( Self : in out Buffer_Tag; elements : out Element_Array ) with pre => elements'Length <= Self.Length;
+   procedure pop_front( Self : in out Buffer_Tag; elements : out Element_Array )
+     with Pre'Class => elements'Length <= Self.Length;
 
-      -- entry pop_front_blocking( Self : in out Buffer_Tag; element : out Element_Type );
-      -- wait until buffer is not empty then read and remove element at front
+   -- entry pop_front_blocking( Self : in out Buffer_Tag; element : out Element_Type );
+   -- wait until buffer is not empty then read and remove element at front
 
-      procedure pop_back( Self : in out Buffer_Tag; element : out Element_Type) with pre => not Self.Empty, post => not Self.Full;
-      -- read and remove element at back
+   procedure pop_back( Self : in out Buffer_Tag; element : out Element_Type)
+     with Pre'Class => not Self.Empty,
+          Post'Class => not Self.Full;
+   -- read and remove element at back
 
-      procedure pop_all( Self : in out Buffer_Tag; elements : out Element_Array )
-         with post => Self.Empty;
-         -- read and remove all elements, front first
+   procedure pop_all( Self : in out Buffer_Tag; elements : out Element_Array )
+     with Post'Class => Self.Empty;
+   -- read and remove all elements, front first
 
-      procedure get_front( Self : in out Buffer_Tag; element : out Element_Type ) with pre => not Self.Empty;
-      -- read element at front
+   procedure get_front( Self : in out Buffer_Tag; element : out Element_Type )
+     with Pre'Class => not Self.Empty;
+   -- read element at front
 
-      procedure get_front( Self : in out Buffer_Tag; elements : out Element_Array ) with pre => elements'Length <= Self.Length;
-      -- read element at front
+   procedure get_front( Self : in out Buffer_Tag; elements : out Element_Array )
+     with Pre'Class => elements'Length <= Self.Length;
+   -- read element at front
 
-      procedure get_back( Self : in out Buffer_Tag; element : out Element_Type ) with pre => not Self.Empty;
-      -- read element at back
+   procedure get_back( Self : in out Buffer_Tag; element : out Element_Type )
+     with Pre'Class => not Self.Empty;
+   -- read element at back
 
-      procedure get_all( Self : in out Buffer_Tag; elements : out Element_Array ) with pre => elements'Length = Self.Length;
-      -- read all elements, front first
+   procedure get_all( Self : in out Buffer_Tag; elements : out Element_Array )
+     with Pre'Class => elements'Length = Self.Length;
+   -- read all elements, front first
 
-      --function get_at( index : Index_Type ) return Element_Type;
+   --function get_at( index : Index_Type ) return Element_Type;
 
-      procedure get_nth_first( Self : in out Buffer_Tag; nth : Index_Type; element : out Element_Type);
-      -- read nth element, nth = 0 is front
+   procedure get_nth_first( Self : in out Buffer_Tag; nth : Index_Type; element : out Element_Type);
+   -- read nth element, nth = 0 is front
 
-      procedure get_nth_last( Self : in out Buffer_Tag; nth : Index_Type; element : out Element_Type);
-      -- read nth element, nth = 0 is back
+   procedure get_nth_last( Self : in out Buffer_Tag; nth : Index_Type; element : out Element_Type);
+   -- read nth element, nth = 0 is back
 
-      function Length( Self : in Buffer_Tag) return Length_Type;
-      -- number of elements in buffer
+   function Length( Self : in Buffer_Tag) return Length_Type;
+   -- number of elements in buffer
 
-      function Empty( Self : in Buffer_Tag) return Boolean;
-      -- true if buffer is empty
+   function Empty( Self : in Buffer_Tag) return Boolean;
+   -- true if buffer is empty
 
-      function hasElements( Self : in Buffer_Tag) return Boolean;
-      -- true if buffer has elements
+   function hasElements( Self : in Buffer_Tag) return Boolean;
+   -- true if buffer has elements
 
-      function Full( Self : in Buffer_Tag) return Boolean;
-      -- true if buffer is full
+   function Full( Self : in Buffer_Tag) return Boolean;
+   -- true if buffer is full
 
-      function Overflows( Self : in Buffer_Tag) return Natural;
-      -- number of buffer overflows
+   function Overflows( Self : in Buffer_Tag) return Natural;
+   -- number of buffer overflows
 
 
 
