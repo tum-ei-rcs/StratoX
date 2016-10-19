23506c42bbe7d4857c43f229f9ae5e2c5b1d095f
emareg <emanuel.regnath@tum.de>
2016-10-19 11:29:49 +0200

    [test] move files

--- software/tests/alternative-units/dimension_test.adb
+++ software/tests/alternative-units/dimension_test.adb
@@ -57,6 +57,7 @@ procedure Dimension_Test is
     -- Generics Tests
     -----------------------------------------
 
+    -- inout paramter
     procedure area( X : in out Translation_Type ) is   -- inout conserves unit
     begin
         null;
@@ -64,34 +65,66 @@ procedure Dimension_Test is
     end area;
 
 
+    -- generic integral
     generic
-        type Area_Type is new Unit_Type;    -- new destroys unit information
-    function areaTwo( X : Translation_Type ) return Area_Type;
+        -- type Integrand_Type is private;   -- ERROR: expected type "Unit_Type"
+        type Integrand_Type is new Unit_Type;   -- ERROR: invalid operand types for operator "*"
+        type Integration_Type is new Unit_Type; 
+        type Integrated_Type is new Unit_Type;    
+    function generic_integral( x : Integrand_Type; t : Integration_Type ) return Integrated_Type;
+    
 
-    function areaTwo( X : Translation_Type ) return Area_Type is 
+    function generic_integral( x : Integrand_Type; t : Integration_Type ) return Integrated_Type is 
     begin
-        -- return X**2;
-        return Area_Type( 2.0 );
-    end areaTwo;
+        --return x * t;    -- Problem: incompatible types, new destroys unit information
+        return Integrated_Type( Unit_Type(x) * Unit_Type(t) );   -- possible, but no unit protection
+    end generic_integral;
+
+    function velocity_integral is new generic_integral(Linear_Velocity_Type, Time_Type, Length_Type);
+
+    my_duration : Time_Type := 30.0 * Second;
+    my_velocity : Linear_Velocity_Type := 4.0 * Meter / Second;
 
 
     -----------------------------------------
     -- Class-Wide Test
     -----------------------------------------
+    package Unit_Class is
+        -- type UnitTest_Tag is tagged Float;  -- ERROR: missing "record"
 
-    type Unit_Tag is tagged record
-        value : Float;
-    end record;
+        type Unit_Tag is tagged record
+            value : Unit_Type'Base;
+        end record   -- aspect "Dimension_System" must apply to numeric derived type declaration
+   --     with Dimension_System =>
+   --      ((Unit_Name => Meter, Unit_Symbol => 'm', Dim_Symbol => 'L'),
+   --       (Unit_Name => Kilogram, Unit_Symbol => "kg", Dim_Symbol => 'M'),
+   --       (Unit_Name => Second, Unit_Symbol => 's', Dim_Symbol => 'T'),
+   --       (Unit_Name => Ampere, Unit_Symbol => 'A', Dim_Symbol => 'I'),
+   --       (Unit_Name => Kelvin, Unit_Symbol => 'K', Dim_Symbol => "Theta"),
+   --       (Unit_Name => Radian, Unit_Symbol => "Rad", Dim_Symbol => "A")),
+   -- Default_Value => 0.0;;
 
-    function "+" (u1 : Unit_Tag'Class; u2 : Unit_Tag'Class) return Unit_Tag is
-    ( ( value => u1.value + u2.value ) );
+        function "+" (u1 : Unit_Tag'Class; u2 : Unit_Tag'Class) return Unit_Tag is
+        ( ( value => u1.value + u2.value ) );
 
 
-    function Add (u1 : Unit_Tag'Class; u2 : Unit_Tag'Class) return Unit_Tag'Class is
-    ( u1 + u2 );
+        function Add (u1 : Unit_Tag'Class; u2 : Unit_Tag'Class) return Unit_Tag'Class is
+        ( u1 + u2 );
+
+        -- dist_c1 : Unit_Tag := ( value => 1.0 * Meter );   -- not possible to asign unit
+        dist_c1 : Unit_Tag := ( value => 1.0 );
+        dist_c2 : Unit_Tag := ( value => 1.0 );
+    end Unit_Class;  -- Dimension System only applies to Numeric Types
+
+    use Unit_Class;
 
 
 
+
+
+--============================================================
+-- Perform Tests
+--============================================================
 begin
 
     -----------------------------------------
@@ -103,8 +136,7 @@ begin
     pos_one := pos_one + pos_two;        -- TODO: make this illegal
 
 
-    Put("Distance: "); Put(distance);
-
+    Put("Distance: "); Put(distance); New_Line;
 
     -----------------------------------------
     -- Spartial Dimension Protection
@@ -118,7 +150,19 @@ begin
 
     el_field := el_field + el_field;
 
-    Put("E-Field X:"); Put(Unit_Type(el_field.x));
+    Put("E-Field X: "); Put(Unit_Type(el_field.x)); New_Line;
 
+
+    -----------------------------------------
+    -- Generics Tests
+    -----------------------------------------
+    distance := velocity_integral(my_velocity, my_duration);
+    Put("Distance: "); Put(distance); New_Line;
+
+    -----------------------------------------
+    -- Class-Wide Test
+    -----------------------------------------
+    dist_c1 := dist_c1 + dist_c2;
+    Put("Class Distance: "); Put(dist_c1.value);
 
 end Dimension_Test;
