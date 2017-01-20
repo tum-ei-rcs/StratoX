90836f5ae6ebfb04dd414f6e06df23d2fa67a809
mbeckersys <mbeckersys@users.noreply.github.com>
2017-01-21 00:52:08 +0100

    [SPARK] prover options revised; added mathsat to portfolio

--- software/prove_all.sh
+++ software/prove_all.sh
@@ -10,14 +10,14 @@ if [ "`hostname`" == "rr-u1204-1" ]; then
 	# server
 	CORES=0 # auto
 	TIMEOU=10000 # make it high, because cvc4 seems to deadlock when timeout triggers
-	PROVERS=cvc4,z3,altergo
+	PROVERS=mathsat,cvc4,z3,altergo
 	PROOF=per_check:all
-	STEPS=100
+	STEPS=1000
 else
 	# laptop etc.
 	CORES=2
 	TIMEOU=auto
-	PROVERS=cvc4,altergo,z3
+	PROVERS=mathsat,cvc4,altergo,z3
 	PROOF=per_check
 	STEPS=10
 	# if you use --level=n, then this overwrites and sets a combination of prover, proof and steps
@@ -31,12 +31,15 @@ rm -f $OBJ/filestats.log
 # do it
 #gprbuild -p -P stratox.gpr
 
+# clean (necessary when above parameters have changed, but increases analysis time!)
+gnatprove -P stratox.gpr --clean
+
 # flow mode
-$TIME gnatprove -XBuild_Mode=Analyze -P stratox.gpr ${PROVEOPTS} -j${CORES} -k --mode=flow --report all --prover${PROVERS} || true
+$TIME gnatprove -XBuild_Mode=Analyze -P stratox.gpr ${PROVEOPTS} -j${CORES} -k --mode=flow --report=all --prover=${PROVERS} || true
 cp $OBJ/gnatprove.out $OBJ/gnatprove_flow.out || true
 
 # prove mode
-$TIME gnatprove -XBuild_Mode=Analyze -P stratox.gpr ${PROVEOPTS} -j${CORES} -k --mode=prove --report provers --prover=${PROVERS} --timeout=${TIMEOU} --proof=${PROOF} --steps=${STEPS} | tee $OBJ/analysis.log || true
+$TIME gnatprove -XBuild_Mode=Analyze -P stratox.gpr ${PROVEOPTS} -j${CORES} -k --mode=prove --report=provers --prover=${PROVERS} --timeout=${TIMEOU} --proof=${PROOF} --steps=${STEPS} | tee $OBJ/analysis.log || true
 cp $OBJ/gnatprove.out $OBJ/gnatprove_prove.out || true
 
 # make statistics
