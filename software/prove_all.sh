cd007cb0c30a319a27b342df74346e2e17ef9525
mbeckersys <mbeckersys@users.noreply.github.com>
2017-01-29 17:15:04 +0100

    [script] generate verification stats also for broken projects

--- software/prove_all.sh
+++ software/prove_all.sh
@@ -5,10 +5,14 @@
 #  1. (optional) prefix for logs
 #  2. (optional) target directory for logs. If empty=obj
 
+# where are the gnatprove outputs and the project file?
 OBJ=../obj/gnatprove
 PRJ=stratox.gpr
+GPFLAGS=-XBuild_Mode=Analyze
 
-echo $0
+# set the following to something non-empty, to analyze all sources individually instead of entire project
+INDIVIDUAL=
+
 
 if [ ! -z "$1" ]; then
     PREFIX="${1}_"
@@ -23,6 +27,34 @@ else
     TAR=$OBJ
 fi
 
+###############
+# FUNCTIONS
+###############
+get_project_units () {
+    rm -f $TAR/_files* $TAR/_units*
+    # compile and get file list through this
+    gprbuild -P${PRJ} -gnatd.n > $TAR/_files.0
+    # filter out lines which are no file paths, and collapse file names to package names
+    for f in `cat $TAR/_files.0`; do
+        if [ -f $f ]; then
+            echo $f >> $TAR/_files.1
+        fi;
+    done
+    # filter out some specific files (e.g., runtime)
+    grep -v ravenscar $TAR/_files.1 | grep -v 'hal/hpl' > $TAR/_files.2
+    # collapse file names to unit names
+    for f in `cat $TAR/_files.2`; do
+        unit=$(basename $f)
+        echo ${unit%.*} >> $TAR/_units.0
+    done
+    # remove duplicates
+    cat $TAR/_units.0 | sort | uniq > $TAR/_units
+    UNITS=$(cat $TAR/_units)
+}
+
+###############
+# PROOF PARAMS
+###############
 TIME="/usr/bin/time -v"
 PROVEOPTS="--pedantic -v" # warns if arithmetic operations could be reorderd, which may refute a proof
 if [ "`hostname`" == "rr-u1204-1" ]; then
@@ -51,20 +83,48 @@ echo "Proving all of $PRJ into $TAR with prefix $PREFIX"
 rm -f $TAR/${PREFIX}gnatprove_flow.out
 rm -f $TAR/${PREFIX}gnatprove_prove.out
 rm -f $TAR/${PREFIX}filestats.log
+rm -f $TAR/${PREFIX}analysis.log
 
+########
 # do it
-#gprbuild -p -P $PRJ
+########
 
-# clean (necessary when above parameters have changed, but increases analysis time!)
+# clean (optional; it is necessary when above parameters have changed, but increases analysis time!)
 gnatprove -P $PRJ --clean
 
-# flow mode
-$TIME gnatprove -XBuild_Mode=Analyze -P $PRJ ${PROVEOPTS} -j${CORES} -k --mode=flow --report=all --prover=${PROVERS} || true
-cp $OBJ/gnatprove.out $TAR/${PREFIX}gnatprove_flow.out || true
+mkdir -p $TAR
 
-# prove mode
-$TIME gnatprove -XBuild_Mode=Analyze -P $PRJ ${PROVEOPTS} -j${CORES} -k --mode=prove --report=provers --prover=${PROVERS} --timeout=${TIMEOU} --proof=${PROOF} --steps=${STEPS} | tee $TAR/${PREFIX}analysis.log || true
-cp $OBJ/gnatprove.out $TAR/${PREFIX}gnatprove_prove.out || true
+if [ ! -z "$INDIVIDUAL" ]; then
+    ##################
+    # iterate files
+    ##################
+    gnatprove -P $PRJ --clean # we definitely need it here
+    get_project_units
+    for u in $UNITS; do        
+        # prove
+        $TIME gnatprove $GPFLAGS -P $PRJ ${PROVEOPTS} -j${CORES} -k --mode=prove --report=provers --prover=${PROVERS} --timeout=${TIMEOU} --proof=${PROOF} --steps=${STEPS} -u $u | tee -a $TAR/${PREFIX}analysis.log || true
+        #echo "Unit=${u}:" >> $TAR/${PREFIX}gnatprove_prove.out
+        #cat $OBJ/gnatprove.out >> $TAR/${PREFIX}gnatprove_prove.out || true
+    done
+    # reports accumulate in gnatprove.out, so we only need to copy once
+    cp $OBJ/gnatprove.out $TAR/${PREFIX}gnatprove_prove.out || true
+else
+    ##################
+    # analyze project
+    ##################
+    
+    # flow mode
+    #$TIME gnatprove $GPFLAGS -P $PRJ ${PROVEOPTS} -j${CORES} -k --mode=flow --report=all --prover=${PROVERS} || true
+    #cp $OBJ/gnatprove.out $TAR/${PREFIX}gnatprove_flow.out || true
 
+    # prove mode
+    $TIME gnatprove $GPFLAGS -P $PRJ ${PROVEOPTS} -j${CORES} -k --mode=prove --report=provers --prover=${PROVERS} --timeout=${TIMEOU} --proof=${PROOF} --steps=${STEPS} | tee $TAR/${PREFIX}analysis.log || true
+    cp $OBJ/gnatprove.out $TAR/${PREFIX}gnatprove_prove.out || true
+fi
+
+##################
 # make statistics
+##################
 ../tools/gnatprove_filestats.py --sort=coverage,success,props --table $TAR/${PREFIX}gnatprove_prove.out $TAR/${PREFIX}analysis.log | tee $TAR/${PREFIX}filestats.log
+
+exit 0
