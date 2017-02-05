1d1bb6c7ac452efd491f28bdfa5e99be25e4ec0a
emareg <emanuel.regnath@tum.de>
2017-02-05 11:55:29 +0100

    [test] output FPU info

--- tests/subnormal/src/main.adb
+++ tests/subnormal/src/main.adb
@@ -25,11 +25,26 @@ procedure main with SPARK_Mode is
    sub64 : Interfaces.IEEE_Float_64 := 2.0E-308;
    sub4 : Interfaces.IEEE_Float_64;
 begin
+
+   -- Float Attributes: http://www.adaic.org/resources/add_content/standards/12rm/html/RM-A-5-3.html
+   Put_Line ("Float Info:");
+   Put_Line ("Machine Radix: "  & Integer'Image(Float'Machine_Radix) & ", Machine Mantissa: " & Integer'Image(Float'Machine_Mantissa));
+   Put_Line ("Machine Exponent: "  & Integer'Image(Float'Machine_Emin) & " .. " & Integer'Image(Float'Machine_Emax));
+   Put_Line ("Machine Denormals: "  & Boolean'Image(machine_has_denorm));
+   Put_Line ("Machine Overflows: " & Boolean'Image(Float'Machine_Overflows));
+   Put_Line ("Machine Rounds: " & Boolean'Image(Float'Machine_Rounds));
+   Put_Line ("Signed Zeros: " & Boolean'Image(Float'Signed_Zeros));
+   Put_Line ("Model Epsilon: " & Float'Image(Float'Model_Epsilon));
+   Put_Line ("Model Small: " & Float'Image(Float'Model_Small));
+   Put_Line ("Model Emin: " & Integer'Image(Float'Model_Emin));
+   Put_Line ("Model Mantissa: " & Integer'Image(Float'Model_Mantissa));
+   Put_Line ("Safe Range: "  & Float'Image(Float'Safe_First) & " .. " & Float'Image(Float'Safe_Last));
+
    pragma Assert (sub1 /= 0.0); -- this is still successful with a denormal
 
    sub4 := 1.12E-309;
    sub4 := Efuncs64.Sin(sub4);
-   Put(IEEE_Float_64'Image(sub4));
+   -- Put(IEEE_Float_64'Image(sub4));
    sub64 := sub64 * sub4;
 
    --res := Efuncs.cos (sub1*sub1);
@@ -40,5 +55,7 @@ begin
    res := sub1 / sub2; -- also okay
    res := sub1 + 1.0; -- also okay
    res := sub1 * sub1;
+   pragma Assert (res in -1.0 .. 1.0);
+
    res := res * sub1;
 end main;
