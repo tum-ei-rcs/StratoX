fc60273ac22bbd5c1340ba8ee9a228266c35a6dd
mbeckersys <mbeckersys@users.noreply.github.com>
2017-02-09 14:12:49 +0100

    [test] some test with Asser_and_cut

--- bug-reports/incorrect-pre-proof/src/main.adb
+++ bug-reports/incorrect-pre-proof/src/main.adb
@@ -1,13 +1,20 @@
 with foo; use foo;
 with Interfaces; use Interfaces;
 
---  GNATprove misses a failed precondition check in the call at line 18
+--  GNATprove GPL 2016 seems to miss a failed precondition check
+--  in the call at line 18. Reason is insufficient knowledge on
+--  others=>, causing a false negative there, which in turn hides
+--  a serious bug.
+--  Fixed in GNATprove Pro 18 (and presumably later in GPL 2017)
 procedure main with SPARK_Mode is
 
    --  inlined callee
    procedure bar (d : out Data_Type)
    is begin
+      --pragma Assert (d'Length > 0);
       d := (others => 0); -- with this, GNATprove does not find violation in line 18
+      pragma Annotate (GNATprove, False_Positive, "length check might fail", "insufficient solver knowledge");
+      pragma Assert_And_Cut (d'Length >= 0);
       --d (d'First) := 0; -- with this, GNATprove indeed finds a violation in line 18
    end bar;
 
